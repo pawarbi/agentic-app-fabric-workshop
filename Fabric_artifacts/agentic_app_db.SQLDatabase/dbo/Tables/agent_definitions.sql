d4f9dddd025c6905ed4c302bc22bea616c96f815
Mehrsa Golestaneh <mehrsa.golestaneh@gmail.com>
2025-12-11 16:13:31 -0500

    Committing 3 items from workspace 7a4f69f2-7876-4c6d-beb7-5840dca2c3d7

--- Fabric_artifacts/agentic_app_db.SQLDatabase/dbo/Tables/agent_definitions.sql
+++ Fabric_artifacts/agentic_app_db.SQLDatabase/dbo/Tables/agent_definitions.sql
@@ -4,7 +4,6 @@ CREATE TABLE [dbo].[agent_definitions] (
     [description]     NVARCHAR (MAX) NULL,
     [llm_config]      NVARCHAR (MAX) NOT NULL,
     [prompt_template] NVARCHAR (MAX) NOT NULL,
-    [agent_type]      VARCHAR (100)  DEFAULT ('specialist') NULL,
     [created_at]      DATETIME2 (7)  DEFAULT (getdate()) NULL,
     PRIMARY KEY CLUSTERED ([agent_id] ASC),
     UNIQUE NONCLUSTERED ([name] ASC)
